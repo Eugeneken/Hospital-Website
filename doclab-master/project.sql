2169c3943b515e4c9bfe3cb8848a86bea48b7898
Eugeneken <keneugene09@gmail.com>
2024-02-07 23:32:50 +0300

    Update the hospital's vision, mission, and strategy on the index page.

---
+++ doclab-master/project.sql
@@ -0,0 +1,37 @@
+SELECT *
+FROM game_sales
+ORDER BY games_sold DESC
+LIMIT 10;
+
+
+SELECT COUNT(*)
+FROM game_sales
+LEFT JOIN reviews 
+ON game_sales.game = reviews.game
+WHERE critic_score IS NULL AND user_score IS NULL;
+
+
+SELECT Year AS release_year, ROUND(AVG(reviews.critic_score), 2) AS avg_critic_score
+FROM game_sales
+JOIN reviews  
+ON game_sales.game = reviews.game
+GROUP BY release_year
+ORDER BY avg_critic_score DESC
+LIMIT 10;
+
+
+SELECT Year AS release_year, ROUND(AVG(reviews.critic_score), 2) AS avg_critic_score, COUNT(game_sales.game) AS num_games
+FROM game_sales
+JOIN reviews  
+ON game_sales.game = reviews.game
+GROUP BY release_year
+HAVING COUNT (game_sales.game) > 4
+ORDER BY avg_critic_score DESC
+LIMIT 10;
+
+
+SELECT *
+FROM release_year
+HAVING plays
+
+
